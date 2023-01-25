a7d1e79ba8b105b222960f2f984d82ae745924db
Sreekanth <sreekanthm549@gmail.com>
2023-01-25 10:00:48 +0530

    feat: completed the eleventh project

--- 50projects-js/project-8/script.js
+++ 50projects-js/project-8/script.js
@@ -3,6 +3,6 @@ const Labels = document.querySelectorAll('.form-control label')
 Labels.forEach(label => {
     label.innerHTML = label.innerText
     .split('')
-    .map((letter, idx) => `<span style= "transition-delay:${idx * 50}ms">${letter}</span>`)
+    .map((letter, idx) => `<span style= "transition-delay:${idx * 80}ms">${letter}</span>`)
     .join('')
 })

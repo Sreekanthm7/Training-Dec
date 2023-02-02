d1717a6a83b0bf2411fa8ecda91d4f902b8794d1
Sreekanth <sreekanthm549@gmail.com>
2023-02-02 14:12:20 +0530

    feat: completed some projects from 50 projects

---
+++ js-fundamentals/arrow-function/script.js
@@ -0,0 +1,17 @@
+// let greet = (user) => {
+//     console.log("hello" + user);
+// }
+// console.log(greet('world'));
+
+
+// let add = (num1,num2) => num1 + num2;
+
+// let result = add(5,6)
+
+// console.log(result)
+
+let myfuncion = (params) => {
+    console.log("hello world");
+}
+
+myfuncion()

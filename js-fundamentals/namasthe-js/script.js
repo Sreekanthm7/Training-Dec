6b35cf2886eb5a98057f011c150920e7be7da9c3
Sreekanth <sreekanthm549@gmail.com>
2023-02-21 09:13:16 +0530

    fix: done some changes in the javascript

--- js-fundamentals/namasthe-js/script.js
+++ js-fundamentals/namasthe-js/script.js
@@ -1,13 +1,47 @@
+// var x = 7;
+// function getName(){
+//     console.log("Namaste Javascript");
+// }
 
+// getName();
 
-var x = 7;
-function getName(){
-    console.log("Namaste Javascript");
+// console.log(x);
+
+// console.log(getName);
+
+// var x = 1;
+
+// a();
+// b();
+// console.log(x);
+
+// function a(){
+//     var x = 10;
+//     console.log(x);
+
+// }
+
+// function b(){
+//     var x = 100;
+//     console.log(x);
+// }
+
+//  function a(){
+//     c();
+//     function c(){
+//         console.log(b);
+
+//     }
+//  }
+
+//  var b = 10;
+//  a()
+
+function a() {
+  var b = 10
+
+  c()
+  function c() {}
 }
-
-
-getName();
-
-console.log(x);
-
-console.log(getName);
+console.log(b)
+a()

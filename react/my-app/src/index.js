7908b5e269a1c5b11581d0fb02ef30873ba2c43c
Sreekanth <sreekanthm549@gmail.com>
2023-02-21 16:02:09 +0530

    fix: done some changes in react

---
+++ react/my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import List from "./list/List";
+
+ReactDOM.render(
+  <div className="app">
+    <Header />
+    <div className="app-body">
+      <List />
+    </div>
+   <Footer />
+  </div>,
+
+  document.getElementById("root")
+)

a92f714a674b29a79aeb44aef4fab302b8d7a055
Sreekanth <sreekanthm549@gmail.com>
2023-02-27 18:33:27 +0530

    fix: done some changes in array methods

---
+++ js-fundamentals/array-methods/script.js
@@ -0,0 +1,50 @@
+// concat()
+
+// const arr1 = ["Cecilie", "Lone"];
+// const arr2 = ["Emil", "Tobias", "Linus"];
+// const arr3 = ["Robin"]
+// const children = arr1.concat(arr2, arr3);
+
+// console.log(children);
+
+// const arr1 = ["Cecilie", "Lone"];
+// const arr2 = [1, 2, 3];
+// const arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+// const arr1  = [1, 2, [3, 4]];
+// const arr2 = [[5, 6], 7, 8];
+// const arr3 = arr1.concat(arr2);
+// console.log(arr3);
+
+// constructor
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let text = fruits.constructors;
+
+// copyWithin()
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.copyWithin(3,0);
+
+// console.log(fruits);
+
+//  const fruits = ["Banana", "Orange", "Apple", "Mango", "Kiwi"];
+//  fruits.copyWithin(2, 0, 2);
+
+//  console.log(fruits);
+
+// entries()
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// const f = fruits.entries();
+
+// for(let x of f){
+//     document.getElementById("demo").innerHTML += x;
+// }
+
+// every()
+
+const ages = [32, 33, 16, 40];
+ages.every

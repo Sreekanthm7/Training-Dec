15755511cbef79f5b8c4b8d21a54c26dc762a45f
Sreekanth <sreekanthm549@gmail.com>
2023-05-08 17:54:57 +0530

    fix: made some changes in substring program

--- java/javaquestions/substring/substring/StringIndex.java
+++ java/javaquestions/substring/substring/StringIndex.java
@@ -16,8 +16,31 @@ public class StringIndex {
     }
 
     public int firstOccurence(char ch) {
-        for(int i = 0; i < s1.length(); i++) {
-            
+        for(int i = 0; i < mainString.length(); i++) {
+            if (ch == mainString.charAt(i)) {
+                return i;
+            }
         }
+        return -1;
+        }
+
+        public int subStringIdx(String subString) {
+            int index = -1;
+            for (int i = 0; i < mainString.length(); i++) {
+                String tempString = "";
+    
+                for (int j = i; j < i + subString.length(); j++) {
+                    tempString += mainString.charAt(j);
+                }
+    
+                if (tempString.equals(subString)) {
+                    index = i;
+                    break;
+                }
+            }
+    
+            return index;
+        }
+
     }
-}
+

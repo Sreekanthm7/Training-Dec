9211c8b1f6f8a289dee96bdf4221c6acc67cb0be
Sreekanthm7 <sreekanthm549@gmail.com>
2023-06-12 18:32:35 +0530

    feat: started type script with net ninja

---
+++ react/type-script/net-ninja-typescript/sandbox.ts
@@ -0,0 +1,67 @@
+// let names = ['luigi', 'mario', 'yoshi']
+// names.push('toad')
+
+
+// let numbers = [10, 20, 30, 40]
+
+// numbers.push(25)
+
+
+// let mixed = ['ken', 4, 'chun-li', 8, 9]
+
+// console.log(mixed);
+
+// mixed.push('ria');
+// mixed.push(10)
+// mixed[0] = 19;
+
+// console.log(mixed);
+
+
+// let ninja = {
+//     name: 'mario',
+//     belt: 'black'
+// }
+
+//explicit types
+
+// let character : string
+// let age: number
+// let isLoggedIn: boolean
+
+// age = 30;
+// isLoggedIn = true;
+
+
+// //arrays
+
+// let ninjas: string[]
+
+// ninjas = ['lufi', 'mario ']
+
+// //union type
+
+// let mixed: (string|number|boolean)[] = [];
+// mixed.push('hello')
+// mixed.push(20)
+// mixed.push(false)
+
+// let uid: string|number;
+// uid = '123',
+// uid = 123
+
+
+// //objects
+
+// let ninjaOne: object;
+// ninjaOne = {name: 'yoshi', age: 30};
+
+
+// let ninjaTwo: {
+//     name: string,
+//     age: number,
+//     beltColour: string
+// }
+
+
+// ninjaTwo = {name: 'shamil', age: 18, beltColour: 'white'}

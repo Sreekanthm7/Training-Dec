3136acd3d941ce28e06daf8e5771c7ce7d5f48a1
Sreekanthm7 <sreekanthm549@gmail.com>
2023-06-15 09:16:43 +0530

    fix: done some typescript programs

---
+++ react/type-script/react-typescript/type-script-tutorial/react-typescript/src/components/Button.tsx
@@ -0,0 +1,8 @@
+type ButtonProps  = {
+    handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void
+}
+
+
+export const Button = (props: ButtonProps) => {
+  return <button onClick={props.handleClick}>Click</button>
+}

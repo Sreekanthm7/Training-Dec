3df068eeae12299b731c33dc2dcf5bb142488c33
Sreekanthm7 <sreekanthm549@gmail.com>
2023-06-23 12:13:56 +0530

    fix: done three array methods sample program

---
+++ js-fundamentals/js-array-methods/find/script.js
@@ -0,0 +1,4 @@
+const array = [5, 6, 7, 8, 9, 10]
+
+const found = array.find(element => element > 6)
+console.log(found);
---
+++ js-fundamentals/js-array-methods/map/script.js
@@ -0,0 +1,4 @@
+const array = [1, 4, 5, 6]
+
+const map = array.map(x => x * 2)
+console.log(map);

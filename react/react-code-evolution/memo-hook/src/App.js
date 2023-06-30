35ff22ee1f7b11900f5824b92c64058b037a0860
Sreekanthm7 <sreekanthm549@gmail.com>
2023-06-30 23:42:10 +0530

    fix: done some changes in the usememo hook

--- react/react-code-evolution/memo-hook/src/App.js
+++ react/react-code-evolution/memo-hook/src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Counter from './components/Counter';
+import logo from "./logo.svg"
+import "./App.css"
+import Counter from "./components/Counter"
 
 function App() {
-  return (
-    <div className="App">
-      <Counter />
-    </div>
-  );
+  return <div className="App">{/* <Counter /> */}</div>
 }
 
-export default App;
+export default App
